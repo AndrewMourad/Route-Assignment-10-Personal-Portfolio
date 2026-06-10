fdbb8d1db0339900d8f591296d5238d6aebaa8a8
AndrewMourad <263699947+AndrewMourad@users.noreply.github.com>
2026-06-10 18:51:01 +0300

    scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,21 @@
-// ^ Write your JavaScript code here
+var sections = document.querySelectorAll("section");
+var navLi = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", function () {
+  var current = "";
+  for (let i = 0; i < sections.length; i++) {
+    var section = sections[i];
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  }
+  for (let i = 0; i < navLi.length; i++) {
+    var li = navLi[i];
+    li.classList.remove("active");
+    if (li.getAttribute("href") === "#" + current) {
+      li.classList.add("active");
+    }
+  }
+});
