29f66fff290285888ec42029e95e1bddc7e46a9e
AndrewMourad <263699947+AndrewMourad@users.noreply.github.com>
2026-06-10 21:02:09 +0300

    Dark Toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,19 @@ window.addEventListener("scroll", function () {
     }
   }
 });
+
+var themeToggle = document.getElementById("theme-toggle-button");
+
+themeToggle.addEventListener("click", function () {
+  document.documentElement.classList.toggle("dark");
+
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+if (localStorage.getItem("theme") === "light") {
+  document.documentElement.classList.remove("dark");
+}
